c39439098548f6a52e7079f2b80715d0d8487d65
csgorun <dimitryi455@gmail.com>
2026-03-12 17:40:00 +0300

    Step 4: variables,data types and calculations

---
+++ code/Step4_TipsCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿//Начало 
+Console.WriteLine("Калькулятор чаевых");
+//Объявление переменных и ввод данных
+Console.Write("Введите сумму счета (руб): ");
+double bill = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите процент чаевых (например, 10): ");
+double tipPercent = Convert.ToDouble(Console.ReadLine());
+//Вычисления 
+double tipAmount = bill * (tipPercent / 100);
+double totalAmount = bill + tipAmount;
+//ВЫвод результата
+Console.WriteLine("\nРезультат:");
+Console.WriteLine($"Сумма счёта: {bill} руб");
+Console.WriteLine($"Чаевые ({tipPercent}%): {tipAmount} руб");
+Console.WriteLine($"Итого к оплате: {totalAmount} руб");
+//Конец
+Console.WriteLine("Спасибо за использование калькулятора!");
