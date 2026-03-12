0af41d950fc2f9cc64699dfa4e4afc5a1f5922c3
csgorun <dimitryi455@gmail.com>
2026-03-12 18:14:24 +0300

    Step 5: multiple conditions with if-else if-else

---
+++ code/Step5_GradeCheck/Program.cs
@@ -0,0 +1,40 @@
+﻿//Начало
+Console.WriteLine("Определение оценки по баллам");
+//Ввод данных
+Console.Write("Введите количество баллок (0-100): ");
+int score = Convert.ToInt32(Console.ReadLine());
+//Переменная для хранения оценки
+string grade;
+int numericGrade;
+//Множественное ветвление
+if (score >= 91 && score < 100)
+{
+    grade = "Отлично";
+    numericGrade = 5;
+} 
+else if (score >= 71 && score < 90)
+{
+    grade = "Хорошо";
+    numericGrade = 4;
+}
+else if (score >= 51 && score < 70)
+{
+    grade = "Удовлетворительно";
+    numericGrade = 3;
+}
+else if (score >= 0 && score < 50)
+{
+    grade = "Неудовлетворительно";
+    numericGrade = 2;
+}
+else
+{
+    grade = "Ошибка";
+    numericGrade = 0;
+    Console.WriteLine("Ошибка: баллы должны быть от 0 до 100");
+    return; 
+}
+//Вывод результата
+Console.WriteLine("Результат");
+Console.WriteLine($"Баллы: {score}");
+Console.WriteLine($"Оценка: {grade} ({numericGrade})");
